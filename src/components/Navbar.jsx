3abe5c1224a812b1f49c93211ce19987e312b9d4
Areum Jo <areumjo1@gmail.com>
2019-08-02 08:52:21 -0400

    MVP completed: activated dark-mode with custom hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import useDarkMode from './hooks/useDarkMode';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
+    console.log('clicked!');
   };
   return (
     <nav className="navbar">
---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState( () => {
+        const item = localStorage.getItem(key);
+        console.log('checking useLocalStorage', key, initialValue, item);
+        return item ? JSON.parse(item) : initialValue;
+    });
+    const setValue = value => {
+        setStoredValue(value);
+        console.log('checking setState', value);
+        localStorage.setItem(key, JSON.stringify(value));
+    };
+    return [storedValue, setValue];
+}
+
+export default useLocalStorage;
